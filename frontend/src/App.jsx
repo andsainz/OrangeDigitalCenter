b9e739e6b1465a06a015f26bf2734afd904fb9df
andsainz <dreasvi@gmail.com>
2023-11-07 15:59:10 +0100

    feat(frontend): post done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import database from "./firebase/firebaseConfig";
-import { ref, onValue, getDatabase } from "firebase/database";
+import { ref, onValue, child, push, update } from "firebase/database";
 import { useState, useEffect } from "react";
 
 
@@ -26,10 +26,31 @@ function App() {
   console.log(activities)
 
   {/* POST */}
+  function writeNewPost() {
+    const db = database;
   
+    // A post entry.
+    const postData = {
+      category: "taller",
+      date: "23/10/23"
+    };
+  
+    // Get a key for a new Post.
+    const newPostKey = push(child(ref(db), 'activities')).key;
+  
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    const updates = {};
+    updates['/activities/' +"activity" ] = postData;
+
+  
+    return update(ref(db), updates);
+  }
+  
+
   return (
     <>
       <h1>¡Hola!</h1>
+      <button onClick={() => {writeNewPost()}}>jola</button>
     </>
   );
 }
