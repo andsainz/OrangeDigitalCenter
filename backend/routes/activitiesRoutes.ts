fa2515d1bf391258908421e74e4599d80c13282f
andsainz <dreasvi@gmail.com>
2023-11-09 22:39:44 +0100

    feat(auth): adding authoritation middleware

--- backend/routes/activitiesRoutes.ts
+++ backend/routes/activitiesRoutes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { getActivities, getActivityById, getActivitiesByCategory, createActivity, updateActivity, deleteActivity} from "../controllers/activitiesController";
+import { getActivities, getActivityById, getActivitiesByCategory, createActivity, updateActivity, deleteActivity } from "../controllers/activitiesController";
+import { authenticateUser, authenticateAdmin } from "../middlewares/authMiddleware";
 
-const router = Router()
+const router = Router();
 
-router.get('/', getActivities)
-router.get('/:id', getActivityById)
-router.get('/categories/:category_id', getActivitiesByCategory)
-router.post('/', createActivity)
-router.put('/:id', updateActivity)
-router.delete('/:id', deleteActivity)
+router.get('/', getActivities);
+router.get('/:id', getActivityById);
+router.get('/categories/:category_id', getActivitiesByCategory);
 
-export default router
+router.use(authenticateUser, authenticateAdmin);
+router.post('/', createActivity);
+router.put('/:id', updateActivity);
+router.delete('/:id', deleteActivity);
+
+export default router;
