8577248ef63473ad4477acd6bbab74666df4eeb7
Cynthia <cyn2993@gmail.com>
2023-11-20 15:41:53 +0100

    feat(footer links): adding footer links in social media icons

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import Twitter from '../../assets/images/twittericon.png';
 import Linkedin from '../../assets/images/linkedinicon.png';
 import Instagram from '../../assets/images/instaicon.png';
 import './Footer.css';
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
   return (
     <footer className="footer">
@@ -17,10 +19,18 @@ const Footer = () => {
         <p>Aviso legal</p>
       </div>
       <div className="footer-icons">
+      <Link to="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
         <img src={Facebook} alt="Icon1" />
+      </Link>
+      <Link to="https://twitter.com/" target="_blank" rel="noopener noreferrer">
         <img src={Twitter} alt="Icon2" />
+      </Link>
+      <Link to="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
         <img src={Linkedin} alt="Icon3" />
+      </Link>
+      <Link to="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
         <img src={Instagram} alt="Icon4" />
+      </Link>
       </div>
     </footer>
   );
