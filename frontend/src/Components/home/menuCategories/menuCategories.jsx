fe41b489f7a0af51546967ff49a049987a3366dd
andsainz <dreasvi@gmail.com>
2023-11-22 11:14:35 +0100

    feat(categories-filter): categories filter done from home view

--- frontend/src/Components/home/menuCategories/menuCategories.jsx
+++ frontend/src/Components/home/menuCategories/menuCategories.jsx
@@ -1,96 +1,31 @@
-/* eslint-disable react/jsx-key */
-import { useState, useEffect } from "react";
-import { Navbar, Nav, Card } from "react-bootstrap";
-import "./MenuCategories.css";
-import "../../home/Cards/Cards.css"
-import { getActivitiesByCategory } from "../../../services/CategoryService";
+import { Navbar, Nav } from 'react-bootstrap';
+import './MenuCategories.css';
+import { activitiesService } from '../../../services/ActivitiesService';
 
-const MenuCategories = () => {
-    const [selectedCategory, setSelectedCategory] = useState(null);
-    const [activities, setActivities] = useState([]);
-    const [error, setError] = useState(null);
+const MenuCategories = ({ onCategoryClick }) => {
+    const categories = [
+        { id: 1, name: "Emprendimiento" },
+        { id: 2, name: "Fabricación digital" },
+        { id: 3, name: "Programación" },
+        { id: 4, name: "Digitalización" },
+        { id: 5, name: "Otros" },
+    ];
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setError(null);
     
-                if (selectedCategory === null) {
-                    // Si no se ha seleccionado una categoría, recuperar todas las actividades
-                    const allActivities = await getActivitiesByCategory(null);
-                    setActivities(allActivities);
-                } else {
-                    // Si se ha seleccionado una categoría, recuperar actividades por esa categoría
-                    const activitiesData = await getActivitiesByCategory(selectedCategory);
-                    console.log("Activities from server:", activitiesData);
-                    setActivities(activitiesData);
-                }
-            } catch (error) {
-                console.error("Error fetching activities:", error);
-                setActivities([]);
-                setError("Error al cargar las actividades.");
-            }
-        };
-    
-        fetchData();
-    }, [selectedCategory]);
-
-    const handleCategoryClick = (category_name) => {
-        setSelectedCategory(category_name);
-    };
-
     return (
-        <div>
-            <Navbar className="menu-categories-navbar" expand="lg">
-                <Nav>
-                    {["emprendimiento", "fabricaciondigital", "programacion", "digitalizacion", "otros"].map(category => (
-                        <Nav.Link
-                            key={category}
-                            href={`#${category}`}
-                            className={`menu-categories-nav-link ${selectedCategory === category && "active"}`}
-                            onClick={() => handleCategoryClick(category)}>
-                            {category.charAt(0).toUpperCase() + category.slice(1)}
-                        </Nav.Link>
-                    ))}
-                </Nav>
-            </Navbar>
-
-            <div className="activities-container">
-                {error ? (
-                    <div>
-                        {/* Resto del contenido del error */}
-                    </div>
-                ) : (
-                    <ul>
-                        {activities.map((activity) => (
-                            <div className="card-section-container">
-                            <Card className="card-section-categories" key={activity.activity_id}>
-                                <Card.Img className="card-home-image" variant="top" src={activity.activity_image} alt="Activity Image" />
-                                <Card.Body className="card-body">
-                                <div className="date-time-container">
-                                    <Card.Title>{activity.activity_date}</Card.Title>
-                                    <Card.Text>{activity.start_time}</Card.Text>
-                                    <Card.Text>{activity.end_time}</Card.Text>
-                                    </div>
-                                <h5>
-                                    <Card.Text>{activity.activity_title}</Card.Text>
-                                </h5>
-                                <Card.Text>{activity.activity_description}</Card.Text>
-                                    <Card.Text>{`${activity.start_time} - ${activity.end_time}`}</Card.Text>
-                                    <button className="read-more-btn">LEER MÁS</button>
-                                </Card.Body>
-                            </Card>
-                            </div>
-                        ))}
-                        {activities.length === 0 && (
-                            <div>
-                                <p className="paragraph-menu-categories">No hay actividades asociadas a esta categoría!</p>
-                            </div>
-                        )}
-                    </ul>
-                )}
-            </div>
-        </div>
+        <Navbar className="menu-categories-navbar" expand="lg">
+            <Nav>
+                {categories.map((category) => (
+                    <Nav.Link
+                        key={category.id}
+                        onClick={() => onCategoryClick(category.name)}
+                        className="menu-categories-nav-link"
+                    >
+                        {category.name}
+                    </Nav.Link>
+                ))}
+            </Nav>
+        </Navbar>
     );
 };
 
