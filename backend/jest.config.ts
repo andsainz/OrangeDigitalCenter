6a00813aee5aebb0699895c7e259f512c4d5d584
andsainz <dreasvi@gmail.com>
2023-11-24 15:15:14 +0100

    feat(tests-backend): adding test backend and modifying jest config

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,16 +1,8 @@
-import type { Config } from 'jest';
-import { defaults } from 'jest-config';
+import type { Config } from '@jest/types';
 
-const config: Config = {
-    transform: { "^.+\\.ts?$": "ts-jest" },
-    roots: ['<rootDir>/tests/'],
-    moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
-    testRegex: '(/_tests_/.*|(\\.|/)(test|spec))\\.(ts?|js?)$',
-    modulePathIgnorePatterns: ['<rootDir>/node_modules'],
-    collectCoverage: true,
-    collectCoverageFrom: ['src/*/.{js,jsx,ts,tsx}', '!src/index.js'],
-    coverageDirectory: 'coverage-jest',
-    coverageReporters: ['text', 'html'],
-    watchPathIgnorePatterns: ['node_modules',],
+const config: Config.InitialOptions = {
     preset: 'ts-jest',
-}
+    testEnvironment: 'node',
+};
+
+export default config;
---
+++ backend/tests/server.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import Server from '../models/server';
+
+let server: Server;
+
+beforeEach(() => {
+    server = new Server();
+});
+
+describe('POST /login', () => {
+    it('should respond with a 200 status code for valid credentials', async () => {
+        const res = await request(server.app)
+            .post('/login')
+            .send({
+                email: 'admin@gmail.com',
+                user_password: '1234',
+            });
+
+        expect(res.status).toBe(200);
+    });
+});
